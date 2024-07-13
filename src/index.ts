34e82b891c7c062917cbbc86961b08789d61a9de
hbybyyang <2451759073@qq.com>
2024-07-14 06:23:15 +0800

    start

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,28 @@
-export function add(a: number, b: number): number {
-  return a + b
+import jwt from 'jsonwebtoken'
+
+export class JWT管理器<JWT负载 extends string | object | Buffer> {
+  constructor(
+    private secret: string,
+    private expiresIn: string,
+  ) {}
+
+  签名(负载: JWT负载): string {
+    var token = jwt.sign(负载, this.secret, {
+      expiresIn: this.expiresIn,
+    })
+    return token
+  }
+
+  解析(token: string | undefined): JWT负载 | undefined {
+    if (token === undefined) {
+      return undefined
+    }
+
+    token = token.replace('Bearer ', '')
+    try {
+      return jwt.verify(token, this.secret) as JWT负载
+    } catch {
+      return undefined
+    }
+  }
 }
